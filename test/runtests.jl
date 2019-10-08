22e5326c1e3a52df937b175324d2d052cb587fb7
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2019-10-08 16:38:20 -0600

    Start validation notebook

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,14 @@ for module_name in ["bertinswing"]
 end
 
 # ------------ TESTS -----------------------------------------------------------
-@test bertin_VLM(; wake_coupled=false, nsteps=1, verbose=true, disp_plot=true)
-@test bertin_VLM(; wake_coupled=true, nsteps=150, verbose=true, disp_plot=true)
+
+# Test VLM solver: Isolated wing
+# @test bertin_VLM(; wake_coupled=false, nsteps=1, verbose=true, disp_plot=true)
+
+# Test VPM+VLM coupling: Isolated wing
+# @test bertin_VLM(; wake_coupled=true, nsteps=150, verbose=true, disp_plot=true)
 # @test bertin_VLM(; wake_coupled=true, nsteps=150, save_path="temps/bertinswing03/", verbose=true)
+@test bertin_VLM(; wake_coupled=!true, nsteps=1, save_path="temps/bertinswing04/", verbose=true)
 
 
 
