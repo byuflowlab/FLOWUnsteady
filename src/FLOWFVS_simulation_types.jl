2d1c4ec3f38275e1ab8b35f39308523c189c3a15
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2019-10-23 15:20:13 -0600

    Simulation type; kinematics visualization

---
+++ src/FLOWFVS_simulation_types.jl
@@ -0,0 +1,145 @@
+#=##############################################################################
+# DESCRIPTION
+    Simulation driver.
+
+# AUTHORSHIP
+  * Author    : Eduardo J. Alvarez
+  * Email     : Edo.AlvarezR@gmail.com
+  * Created   : Oct 2019
+  * License   : MIT
+=###############################################################################
+
+mutable struct Simulation{V<:AbstractVehicle, M<:AbstractManeuver, R<:Real}
+    # USER INPUTS: Simulation setup
+    vehicle::V              # Vehicle
+    maneuver::M             # Maneuver to be performed
+    Vref::R                 # Reference velocity in this maneuver
+    RPMref::R               # Reference RPM in this maneuver
+    ttot::R                 # Total time in which to perform the maneuver
+
+
+    # INTERNAL PROPERTIES: Runtime parameters
+    t::R                    # Dimensional time
+    nt::Int                 # Current time step number
+
+    Simulation{V, M, R}(
+                            vehicle, maneuver, Vref, RPMref, ttot;
+                            t=zero(R), nt=0
+                        ) where {V, M, R} = check(vehicle, maneuver) ? new(
+                            vehicle, maneuver, Vref, RPMref, ttot,
+                            t, nt
+                        ) : nothing
+end
+
+
+# Implicit V and M constructor
+Simulation(v::AbstractVehicle, m::AbstractManeuver, n::Real, args...
+                 ) = Simulation{typeof(v), typeof(m), typeof(n)}(v, m, n, args...)
+
+"""
+Checks that vehicle and maneuver are compatible.
+"""
+function check(vehicle::AbstractVehicle, maneuver::AbstractManeuver;
+                                                            raise_error=true)
+    res = get_ntltsys(vehicle)==get_ntltsys(maneuver)
+    res *= get_nrtrsys(vehicle)==get_nrtrsys(maneuver)
+
+    if raise_error && res==false
+        error("Encountered incompatible Vehicle and Maneuver!")
+    end
+
+    return res
+end
+
+"""
+Takes a kinematic time step `dt` where the new velocity and angular velocity
+is calculated and the vehicle is translated and rotated according to it. It also
+updates the tilt angle and RPM of every system.
+
+NOTE: No solver is run in this process, rather it uses the current aerodynamic
+solution to calculate acceleration and moment.
+"""
+function nextstep_kinematic(self::Simulation, dt::Real)
+
+    # Linear velocity increment
+    dV = calc_dV(self.maneuver, self.vehicle, self.t, dt, self.ttot, self.Vref)
+
+    # Angular velocity increment
+    dW = calc_dW(self.maneuver, self.vehicle, self.t, dt, self.ttot)
+
+    # Update vehicle linear and angular velocity
+    add_dV(self.vehicle, dV)
+    add_dW(self.vehicle, dW)
+
+    # Translates and rotates the vehicle according to current velocity
+    nextstep_kinematic(self.vehicle, dt)
+
+    # TODO: Update tilt angles
+    # TODO: Have the rotor solver update rotor RPMs
+
+
+    self.t += dt
+    self.nt += 1
+end
+
+
+function save_vtk(self::Simulation, filename; optargs...)
+    return save_vtk(self.vehicle, filename; num=self.nt, optargs...)
+end
+
+
+
+
+
+
+
+function nextstep(self::Simulation, dt::Real)
+
+    # ---------- 0) TRANSLATION AND ROTATION OF SYSTEM -----------------
+    t = (self.t+dt)/self.ttot              # Non-dimensional time
+
+    # Translation of aircraft
+    V = self.Vref * Vaircraft(t)
+    dX = V*DT
+
+    vlm.setcoordsystem(system, system.O + dX, system.Oaxis)
+    vlm.vtk.lintransform!(fuselage, eye(3), dX)
+
+    O_fuselage .+=  dX
+
+    # Rotation of tilting systems and aircraft (dangles[end] is aircraft)
+    new_angles = [angles(t)...]
+    dangles = new_angles - prev_angles
+
+    for (j, dangle) in enumerate(dangles)
+
+        Oaxis = vlm.vtk.rotation_matrix2([-a for a in dangle]...)
+
+        if j != size(dangles, 1)        # Titlting systems
+            sys = tilting_systems[j]
+        else                            # Aircraft and fuselage
+            sys = system
+            vlm.vtk.lintransform!(fuselage, eye(3), -O_fuselage)
+            vlm.vtk.lintransform!(fuselage, Oaxis, O_fuselage)
+        end
+
+        vlm.setcoordsystem(sys, sys.O, Oaxis*sys.Oaxis)
+    end
+
+    prev_angles[:] = new_angles
+
+
+    # Rotation of rotors
+    rpms = RPMs(t)
+    cur_RPMs = Dict()
+    for j in 1:size(rpms, 1)
+        for rotor in rotors_systems[j]
+            rpm = rpms[j] * (rand_RPM ? 1 + (rand()-0.5)*0.1 : 1.0)
+            rotation = 360*(RPMh_w*rpm)/60 * DT
+            vlm.rotate(rotor, rotation)
+
+            cur_RPMs[rotor] = RPMh_w*rpm
+        end
+    end
+
+end
---
+++ src/FLOWFVS_utils.jl
@@ -0,0 +1,221 @@
+#=##############################################################################
+# DESCRIPTION
+    Utilities
+
+# AUTHORSHIP
+  * Author    : Eduardo J. Alvarez
+  * Email     : Edo.AlvarezR@gmail.com
+  * Created   : Oct 2019
+  * License   : MIT
+=###############################################################################
+
+# Color pallete for plots
+clrs = [
+            # [17, 20, 76]/255,
+            [58, 150, 144]/255,
+            [250, 188, 96]/255,
+            [225, 98, 98]/255,
+            [194, 232, 206]/255,
+            [148, 170, 42]/255,
+            [42, 148, 170]/255,
+            [170, 42, 84]/255,
+            [113, 145, 146]/255,
+            [252, 127, 178]/255
+        ]
+
+function plot_maneuver(maneuver::KinematicManeuver;
+                        ti::Real=0, tf::Real=1, vis_nsteps=300,
+                        figname="maneuver", tstages=[])
+
+    Vvhcl = maneuver.Vvehicle
+    avhcl = maneuver.anglevehicle
+    ts = linspace(ti, tf, vis_nsteps)
+
+    # -------------------- Vehicle velocity history ----------------------------
+    figure(figname*"-kinematics", figsize=[7*3, 5*1])
+    suptitle("VEHICLE KINEMATICS")
+
+    subplot(131)
+    title("Velocity")
+    Vs = Vvhcl.(ts)
+    Vmax = max([maximum([V[i] for V in Vs]) for i in 1:3]...)
+    Vmin = min([minimum([V[i] for V in Vs]) for i in 1:3]...)
+
+    plot(ts, [V[1] for V in Vs], "-", label=L"V_x", alpha=0.8, color=clrs[1])
+    plot(ts, [V[2] for V in Vs], "-", label=L"V_y", alpha=0.8, color=clrs[2])
+    plot(ts, [V[3] for V in Vs], "-", label=L"V_z", alpha=0.8, color=clrs[3])
+
+    for tstage in tstages
+        plot(tstage*ones(2), [Vmin, Vmax], ":k", alpha=0.5)
+    end
+
+    xlabel("Non-dimensional time")
+    ylabel("Non-dimensional velocity")
+    legend(loc="best", frameon=false)
+
+    # -------------------- Mission profile -------------------------------------
+    Xinit = zeros(3)        # Initial position
+    dt = ts[2]-ts[1]        # Time step
+
+    Xs = [Xinit]
+    for i in 1:(size(ts,1)-1)
+        push!(Xs, Xs[end] + dt*Vs[i])
+    end
+
+    subplot(132)
+    title("Position")
+    Xmax = max([maximum([X[i] for X in Xs]) for i in 1:3]...)
+    Xmin = min([minimum([X[i] for X in Xs]) for i in 1:3]...)
+
+    plot(ts, [X[1] for X in Xs], "-g", label=L"x", alpha=0.8, color=clrs[1])
+    plot(ts, [X[2] for X in Xs], "-b", label=L"y", alpha=0.8, color=clrs[2])
+    plot(ts, [X[3] for X in Xs], "-r", label=L"z", alpha=0.8, color=clrs[3])
+
+    for tstage in tstages
+        plot(tstage*ones(2), [Xmin, Xmax], ":k", alpha=0.5)
+    end
+
+    xlabel("Non-dimensional time")
+    ylabel("Non-dimensional position")
+    legend(loc="best", frameon=false)
+
+    # -------------------- Vehicle angle history -------------------------------
+    subplot(133)
+    title("Angles")
+    as = avhcl.(ts)
+    amax = max([maximum([a[i] for a in as]) for i in 1:3]...)
+    amin = min([minimum([a[i] for a in as]) for i in 1:3]...)
+
+    plot(ts, [a[1] for a in as], "-g", label=L"\theta_x", alpha=0.8, color=clrs[1])
+    plot(ts, [a[2] for a in as], "-b", label=L"\theta_y", alpha=0.8, color=clrs[2])
+    plot(ts, [a[3] for a in as], "-r", label=L"\theta_z", alpha=0.8, color=clrs[3])
+
+    for tstage in tstages
+        plot(tstage*ones(2), [amin, amax], ":k", alpha=0.5)
+    end
+
+    xlabel("Non-dimensional time")
+    ylabel(L"Angle ($^\circ$)")
+    legend(loc="best", frameon=false)
+
+
+
+    # -------------------- Tilting system history ------------------------------
+    figure(figname*"-controls", figsize=[7*2, 5*2])
+    suptitle("VEHICLE CONTROLS")
+
+    angle_syss = [a.(ts) for a in maneuver.angle]    # Angles of every tilt sys
+    for i in 1:3
+        subplot(220+i)
+
+         # i-th angle of every tilting system
+        a_syss = [[a[i] for a in angle_sys] for angle_sys in angle_syss]
+
+        amax = max([maximum(a_sys) for a_sys in a_syss]...)
+        amin = min([minimum(a_sys) for a_sys in a_syss]...)
+
+        for (j, a_sys) in enumerate(a_syss)
+            plot(ts, a_sys, "-", label="Tilt-sys #$j", alpha=0.8,
+                                            color=clrs[(j-1)%length(clrs) + 1])
+        end
+
+        xlabel("Non-dimensional time")
+        ylabel("Angle "*(i==1? L"\theta_x" : i==2 ? L"\theta_y" : L"\theta_z") * L" ($^\circ$)")
+        legend(loc="best", frameon=false)
+    end
+
+    # -------------------- Rotor systems history -------------------------------
+    RPM_syss = [rpm.(ts) for rpm in maneuver.RPM]    # RPM of every rotor system
+    subplot(224)
+
+    RPMmax = max([maximum(RPM_sys) for RPM_sys in RPM_syss]...)
+    RPMmin = min([minimum(RPM_sys) for RPM_sys in RPM_syss]...)
+
+    for (j, RPM_sys) in enumerate(RPM_syss)
+        plot(ts, RPM_sys, "-", label="Rotor-sys #$j", alpha=0.8,
+                                        color=clrs[(j-1)%length(clrs) + 1])
+    end
+
+    xlabel("Non-dimensional time")
+    ylabel("Non-dimensional RPM (RPM/RPMh)")
+    legend(loc="best", frameon=false)
+
+end
+
+
+function visualize_kinematics(sim::Simulation{V, KinematicManeuver{N, M}, R},
+                                nsteps::Int, save_path::String;
+                                run_name="vis",
+                                prompt=true,
+                                verbose=true, v_lvl=0, verbose_nsteps=10,
+                                paraview=true,
+                                save_vtk_optsargs=[],
+                              ) where {V<:AbstractVehicle, R<:Real, N, M}
+
+
+    dt = sim.ttot/nsteps                # Time step
+
+    # Create save path
+    gt.create_path(save_path, prompt)
+    strn = ""
+
+    if verbose
+        println("\t"^(v_lvl)*"*"^(73-7*v_lvl))
+        println("\t"^(v_lvl)*"START $(joinpath(save_path,run_name))")
+        println("\t"^(v_lvl)*"*"^(73-7*v_lvl))
+
+        figure(run_name, figsize=[7*2, 5*1])
+        subplot(121)
+        xlabel("Simulation time")
+        ylabel("Velocity")
+        Vlbls = [L"V_x", L"V_y", L"V_z"]
+        subplot(122)
+        xlabel("Simulation time")
+        ylabel(L"Angular velocity ($^\circ/t$)")
+        Wlbls = [L"\Omega_x", L"\Omega_y", L"\Omega_z"]
+    end
+
+    # Time stepping
+    for i in 0:nsteps
+
+        if i!=0
+            nextstep_kinematic(sim, dt)
+        end
+
+        # Verbose
+        if verbose && i%verbose_nsteps==0
+            println("\t"^(v_lvl+1)*"Time step $i out of $nsteps")
+        end
+        if verbose
+            for j in 1:3
+                subplot(121)
+                plot(sim.t, sim.vehicle.V[j], ".", label=Vlbls[j], alpha=0.8,
+                                                                color=clrs[j])
+                subplot(122)
+                plot(sim.t, sim.vehicle.W[j], ".", label=Wlbls[j], alpha=0.8,
+                                                                color=clrs[j])
+            end
+            if i==0
+                for j in 1:2
+                    subplot(120+j)
+                    legend(loc="best", frameon=false)
+                end
+            end
+        end
+
+        strn = save_vtk(sim, run_name; path=save_path, save_vtk_optsargs...)
+    end
+
+    # Tweak vtk string to be a time sequence
+    if nsteps>1
+        strn = replace(strn, ".$(nsteps).", "...")
+    end
+
+    # Call paraview
+    if paraview
+        if verbose; println("\t"^(v_lvl)*"Calling Paraview..."); end
+        run(`paraview --data="$save_path/$strn"`)
+    end
+
+    return strn
+end
