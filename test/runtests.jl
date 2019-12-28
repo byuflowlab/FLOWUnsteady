50afdd1864a071ec4e7e44d7d2a7d7b9c3555de7
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2019-12-27 21:18:03 -0700

    Add wake-decoupled solver; update unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,21 +18,20 @@ end
 
 # ------------ TESTS -----------------------------------------------------------
 
-# # Test VLM solver: Isolated wing
-# @test bertin_VLM(; wake_coupled=false, nsteps=0, verbose=true, disp_plot=true)
-#
-# # Test VLM regularization: Isolated wing
-# @test bertin_VLM(; wake_coupled=false, vlm_fsgm=0.00125, nsteps=0, verbose=true, disp_plot=true)
+# Test VLM solver: Isolated wing
+@test bertin_VLM(; wake_coupled=false, nsteps=0, verbose=true, disp_plot=true)
+
+# Test VLM regularization: Isolated wing
+@test bertin_VLM(; wake_coupled=false, vlm_fsgm=0.00125, nsteps=0, verbose=true, disp_plot=true)
 
 # Test VPM+VLM coupling: Isolated wing
-# @test bertin_VLM(; wake_coupled=true, nsteps=150, verbose=true, disp_plot=true)
-@test bertin_VLM(; wake_coupled=true, nsteps=150, save_path="temps/bertinswing98/", verbose=true)
+@test bertin_VLM(; wake_coupled=true, nsteps=150, verbose=true, disp_plot=true)
 
-# # Test VPM+VLM solver on kinematic velocity: Isolated wing
-# @test bertin_kinematic(; nsteps=150, verbose=true, disp_plot=true)
-#
-# # Test simulation stability: Isolated wing with a very fine time stepping and shedding
-# @test bertin_kinematic(; nsteps=300, p_per_step=4, vlm_rlx=0.75, verbose=true, disp_plot=true)
+# Test VPM+VLM solver on kinematic velocity: Isolated wing
+@test bertin_kinematic(; nsteps=150, verbose=true, disp_plot=true)
+
+# Test simulation stability: Isolated wing with a very fine time stepping and shedding
+@test bertin_kinematic(; nsteps=300, p_per_step=4, vlm_rlx=0.75, verbose=true, disp_plot=true)
 
 
 # @test bertin_kinematic(; nsteps=150, p_per_step=4, vlm_rlx=0.75, save_path="temps/bertinswing11/", verbose=true, disp_plot=true)
