9c85b489bfb726e8f8f577e61950451dc9a0341c
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2020-01-10 17:44:21 -0700

    Adjust fluid domain

--- examples/blownwing/blownwing_postprocessing.jl
+++ examples/blownwing/blownwing_postprocessing.jl
@@ -26,13 +26,13 @@ function postprocessing_fluiddomain(read_path::String, nums, save_path::String;
     if verbose; println("\t"^(v_lvl)*"Creating grid..."); end;
     lx = 1.00*b                         # Length in x-direction
     ly = 1.10*b                         # Length in y-direction
-    lz = 2.30*R                         # Length in z-direction
+    lz = 2.35*R                         # Length in z-direction
 
     dx = R/ndx                          # Element size in x-direction
     dy = dx                             # Element size in y-direction
     dz = dx                             # Element size in z-direction
 
-    P_min = [0.0, -ly/2, -lz/2]         # Minimum bounding point of grid
+    P_min = [0.0, -ly/2, -lz/2*1.1]         # Minimum bounding point of grid
     P_max = P_min + [lx, ly, lz]        # Maximum bounding point of grid
 
     NDIVS = ceil.(Int, [lx/dx, ly/dy, lz/dz])   # Number of cells in each direction
@@ -55,6 +55,15 @@ function postprocessing_fluiddomain(read_path::String, nums, save_path::String;
         # Read particle field
         pfield = fvs.vpm.read_vtk(run_name*"_pfield", Vinf, nothing; path=read_path,
                                                 num=num, solver_method="ExaFMM")
+
+        # Eliminates particles outside the domain to save computation
+        xmax = gt.get_node(grid, NDIVS+1)[1]
+        for p in fvs.vpm.get_np(pfield):-1:1
+            if fvs.vpm.get_x(pfield, p)[1]>xmax
+                fvs.vpm.delparticle(pfield, p)
+            end
+        end
+
         if verbose; println("\t$(fvs.vpm.get_np(pfield)) particles"); end;
 
         # Evaluate particle field velocity
