27955c357cc836a42e0f6a32ec40a59d02810e11
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2020-04-11 17:36:54 -0600

    Code quasi-steady VLM solver

--- src/FLOWUnsteady_simulation_types.jl
+++ src/FLOWUnsteady_simulation_types.jl
@@ -55,6 +55,10 @@ Simulation(v::AbstractVehicle, m::AbstractManeuver, n::Real, args...; optargs...
                                                                      optargs...)
 
 
+# Solvers of concrete AbstractVehicle implementations
+for header_name in ["unsteady", "quasisteady"]
+  include("FLOWUnsteady_simulation_types_"*header_name*".jl")
+end
 
 
 ##### FUNCTIONS  ###############################################################
@@ -127,181 +131,8 @@ function precalculations(self::Simulation, Vinf::Function,
     return nothing
 end
 
-function solve(self::Simulation{V, M, R}, Vinf::Function,
-                pfield::vpm.ParticleField, wake_coupled::Bool,
-                vpm_solver::String, dt::Real, rlx::Real, sigma::Real, rho::Real,
-                speedofsound::Real; init_sol::Bool=false
-                ) where {V<:VLMVehicle, M<:AbstractManeuver, R}
-
-
-    # Bring the rotors back to their initial positions at beginning of this
-    # time step before `precalculations`. This is needed to avoid double
-    # accounting for the RPM when the solver is run
-    rotate_rotors(self, -dt)
-
-    vhcl = self.vehicle
-    mnvr = self.maneuver
-    t = self.t
-
-    # TIME-STEPPING PROCEDURE:
-    # -1) Solve one particle field time step
-    # 0) Translate and rotate systems
-    # 1) Recalculate horseshoes with kinematic velocity
-    # 2) Paste previous Gamma solution to new system position after translation
-    # 3) Shed semi-infinite wake after translation
-    # 4) Calculate wake-induced velocity on VLM and Rotor system
-    # 5) Solve VLM and Rotor system
-    # 6) Shed unsteady-loading wake after new solution
-    # 7) Save new solution as prev solution
-    # Iterate
-    #
-    # On the first time step (pfield.nt==0), it only does steps (5) and (7),
-    # meaning that the unsteady wake of the first time step is never shed.
-    if t==0
-        # NOTE: VLMs and Rotors solutions are losely coupled
-
-        # Set Vinf on system to get horseshoes
-        vlm.setVinf(vhcl.system, Vinf)
-
-        # Solve VLMs
-        # TODO: Add Rotor-on-VLM induced velocity
-        if init_sol
-            # NOTE: Here I use the semi-infinite wake for the first step, which is
-            # may lead to some unphysical results when shedding unsteady loading wake
-            vlm.solve(vhcl.vlm_system, Vinf; t=t, keep_sol=true)
-        else
-            vlm.solve(vhcl.vlm_system, Vinf; t=t, keep_sol=true,
-                                                    vortexsheet=(X,t)->zeros(3))
-        end
-
-        # Calculate induced velocities to use in rotor solver
-        ## Points where to calculate induced velocities
-        Xs = _get_midXs(vhcl.rotor_systems)
-
-        ## Particles for VLM-on-Rotor induced velocity
-        static_particles = _static_particles(vhcl.vlm_system, sigma)
-
-        ## Evaluate VPM-on-Rotor induced velocity + static particles
-        Vinds = Vvpm_on_Xs(pfield, Xs, vpm_solver;
-                                            static_particles=static_particles)
-
-        # Solve Rotors
-        for (si, rotors) in enumerate(vhcl.rotor_systems)
-
-            # RPM of this rotor system
-            RPM = self.RPMref*get_RPM(mnvr, si, t/self.ttot)
-
-            for (ri, rotor) in enumerate(rotors)
-
-                # Get velocities induced at every blade of this rotor
-                this_Vinds = _parse_midXs(vhcl.rotor_systems,  Vinds, si, ri)
-
-                VindVkin = _format_blades(this_Vinds, vhcl.rotor_systems, si, ri)
-
-                if wake_coupled
-                    vlm.solvefromV(rotor, VindVkin, Vinf, RPM,
-                                  rho; t=t, sound_spd=speedofsound)
-                else
-                    vlm.solvefromCCBlade(rotor, Vinf, RPM, rho;
-                                              t=t, sound_spd=speedofsound)
-                end
-            end
-        end
-
-    else
-        # ---------- 4) Calculate VPM velocity on VLM and Rotor system -----
-        # Control points of VLM
-        Xs = _get_Xs(vhcl.vlm_system, "CP"; t=t)
-
-        # Generate static particles
-        static_particles = Array{Float64, 1}[]
-
-        ### Particles for Rotor induced velocity
-        prev_rotor_systems = _get_prev_rotor_systems(vhcl)
-
-        for (si, rotors) in enumerate(vhcl.rotor_systems)
-            for (ri, rotor) in enumerate(rotors)
-
-                vlm.getHorseshoe(rotor, 1)          # Force HS calculation
-                vlm._addsolution(rotor, "Gamma",    # Give previous solution
-                                prev_rotor_systems[si][ri]._wingsystem.sol["Gamma"])
-                _static_particles(rotor, sigma; out=static_particles)
-
-            end
-        end
-
-        # VPM-induced velocity (+ static particles) at every VLM control point
-        Vvpm = Vvpm_on_Xs(pfield, Xs, vpm_solver; static_particles=static_particles)
-        vlm._addsolution(vhcl.vlm_system, "Vvpm", Vvpm; t=t)
-
-
-        # Calculate induced velocities to use in rotor solver
-        ## Points where to calculate induced velocities
-        Xs = _get_midXs(vhcl.rotor_systems)
-
-        ### Particles for VLM-on-Rotor induced velocity
-        # NOTE: If I keep the rotor particles wouldn't I be double-counting
-        # the blade induced velocity and make the solution unstable?
-        _static_particles(vhcl.vlm_system, sigma; out=static_particles)
-
-        ## Evaluate VPM-on-Rotor induced velocity + static particles
-        Vinds = Vvpm_on_Xs(pfield, Xs, vpm_solver;
-                                            static_particles=static_particles)
-
-        # ---------- 5) Solve VLM system -----------------------------------
-        # Wake-coupled solution
-        if wake_coupled
-            vlm.solve(vhcl.vlm_system, Vinf; t=t, extraVinf=_extraVinf2,
-                                    keep_sol=true, vortexsheet=(X,t)->zeros(3))
-        # Wake-decoupled solution
-        else
-            vlm.solve(vhcl.vlm_system, Vinf; t=t, extraVinf=_extraVinf1,
-                                                                keep_sol=true)
-        end
-
-        # Relaxes (rlx->1) or stiffens (rlx->0) the VLM solution
-        if rlx > 0
-            rlxd_Gamma = rlx*vhcl.vlm_system.sol["Gamma"] +
-                                (1-rlx)*_get_prev_vlm_system(vhcl).sol["Gamma"]
-            vlm._addsolution(vhcl.vlm_system, "Gamma", rlxd_Gamma)
-        end
-
-        # ---------- 5) Solve Rotor system ---------------------------------
-
-        # Solve Rotors
-        for (si, rotors) in enumerate(vhcl.rotor_systems)
-
-            # RPM of this rotor system
-            RPM = self.RPMref*get_RPM(mnvr, si, t/self.ttot)
-
-            for (ri, rotor) in enumerate(rotors)
-
-                # Calculate kinematic velocities
-                Vkin = _Vkinematic_rotor(vhcl.rotor_systems,
-                                         prev_rotor_systems, si, ri, dt)
-
-                # Get velocities induced at every blade of this rotor
-                this_Vinds = _parse_midXs(vhcl.rotor_systems, Vinds, si, ri)
-
-                VindVkin = _format_blades(this_Vinds + Vkin, vhcl.rotor_systems,
-                                                                        si, ri)
-                if wake_coupled
-                    vlm.solvefromV(rotor, VindVkin, Vinf, RPM,
-                                  rho; t=t, sound_spd=speedofsound)
-                else
-                    vlm.solvefromCCBlade(rotor, Vinf, RPM, rho;
-                                                t=t, sound_spd=speedofsound)
-                end
-            end
-        end
-
-        # Rotate rotors
-        rotate_rotors(self, dt)
-    end
-end
-
 function rotate_rotors(self::Simulation{V, M, R}, dt::Real
-                                ) where {V<:VLMVehicle, M<:AbstractManeuver, R}
+                        ) where {V<:AbstractVLMVehicle, M<:AbstractManeuver, R}
 
     for (si, rotors) in enumerate(self.vehicle.rotor_systems)
 
---
+++ src/FLOWUnsteady_vehicle_vlm_quasisteady.jl
@@ -0,0 +1,151 @@
+#=##############################################################################
+# DESCRIPTION
+    Vehicle with vortex-lattice method (VLM) and blade-element momentum (BEM)
+    models assuming quasi-steady wakes. This means that at every time step the
+    wake of VLM models are represented as rigid, semi-infinite filaments, and
+    wakes of BEM models are obtained from a conservation of momentum
+    assumptions. In this approach only VLM-on-VLM and VLM-on-BEM interactions
+    are accounted for, meaning that the wake of each BEM rotor has no effect
+    on other rotors and wings.
+
+# AUTHORSHIP
+  * Author    : Eduardo J. Alvarez
+  * Email     : Edo.AlvarezR@gmail.com
+  * Created   : Apr 2020
+  * License   : MIT
+=###############################################################################
+
+################################################################################
+# QUASI-STEADY VLM VEHICLE TYPE
+################################################################################
+"""
+    `QVLMVehicle(system; optargs...)`
+
+Type handling all geometries and subsystems that define a flight vehicle made
+out of VLM (Wing, WingSystem, Rotor) components.
+
+NOTE: For the solver to work correctly, all components in `wake_system` (if any)
+need to be also components of `vlm_system`.
+
+# ARGUMENTS
+* `system::vlm.WingSystem`:        System of all FLOWVLM objects. This system
+                                    is considered as the entire vehicle. Not all
+                                    components in this system will be solved,
+                                    but they will all be rotated and translated
+                                    during maneuver.
+# OPTIONAL ARGUMENTS
+* `tilting_systems::Tuple(vlm.WingSystem, ...)`:   Tuple of all FLOWVLM
+                                    tilting objects, where `tilting_systems[i]`
+                                    contains the i-th FLOWVLM system of lifting
+                                    surfaces and rotors that tilt together.
+* `rotors_systems::Tuple(Array{vlm.Rotor,1}, ...)`:   Tuple of groups of Rotors
+                                    that share a common RPM.
+* `vlm_system::vlm.WingSystem`:    System of all FLOWVLM objects to be solved
+                                    through the VLM solver.
+* `wake_system::vlm.WingSystem`:   System of all FLOWVLM objects that will
+                                    have a rigid, semi-infinite VLM wake.
+* `grids::Array{gt.GridTypes, 1}`: Array of grids that will be translated and
+                                    rotated along with `system`.
+"""
+struct QVLMVehicle{N, M, R} <: AbstractVLMVehicle{N, M, R}
+
+    # Required inputs
+    system::vlm.WingSystem
+
+    # Optional inputs
+    tilting_systems::NTuple{N, vlm.WingSystem}
+    rotor_systems::NTuple{M, Array{vlm.Rotor, 1}}
+    vlm_system::vlm.WingSystem
+    wake_system::vlm.WingSystem
+    grids::Array{gt.GridTypes, 1}
+
+    # Internal properties
+    V::Array{R, 1}                          # Current vehicle velocity
+    W::Array{R, 1}                          # Current vehicle angular velocity
+    prev_data::Array{Any, 1}                # Information about previous step
+    grid_O::Array{Array{R, 1}, 1}           # Origin of every grid
+
+
+    QVLMVehicle{N, M, R}(
+                    system;
+                    tilting_systems=NTuple{0, vlm.WingSystem}(),
+                    rotor_systems=NTuple{0, Array{vlm.Rotor, 1}}(),
+                    vlm_system=vlm.WingSystem(),
+                    wake_system=vlm.WingSystem(),
+                    grids=Array{gt.GridTypes, 1}(),
+                    V=zeros(3), W=zeros(3),
+                    prev_data=[deepcopy(vlm_system), deepcopy(wake_system),
+                                                    deepcopy(rotor_systems)],
+                    grid_O=Array{Array{Float64, 1}, 1}(),
+                ) where {N, M, R} = new(
+                    system,
+                    tilting_systems,
+                    rotor_systems,
+                    vlm_system,
+                    wake_system,
+                    grids,
+                    V, W,
+                    prev_data,
+                    grid_O,
+                )
+end
+
+# Implicit N and M constructor
+QVLMVehicle(system::vlm.WingSystem;
+        V::Array{R, 1}=zeros(3), W::Array{R, 1}=zeros(3),
+        tilting_systems::NTuple{N, vlm.WingSystem}=NTuple{0, vlm.WingSystem}(),
+        rotor_systems::NTuple{M, Array{vlm.Rotor, 1}}=NTuple{0, Array{vlm.Rotor, 1}}(),
+        grids=Array{gt.GridTypes, 1}(),
+        optargs...
+        ) where {N, M, R} = QVLMVehicle{N, M, R}( system;
+                                V=V, W=W,
+                                tilting_systems=tilting_systems,
+                                rotor_systems=rotor_systems,
+                                grids=Array{gt.GridTypes, 1}(grids),
+                                grid_O=[zeros(R, 3) for i in 1:length(grids)],
+                                optargs...)
+
+
+##### FUNCTIONS  ###############################################################
+function shed_wake(self::QVLMVehicle, Vinf::Function, pfield::vpm.ParticleField,
+                                                        dt::Real; optargs...)
+    nothing
+end
+
+function generate_static_particle_fun(self::QVLMVehicle, sigma::Real)
+
+    function static_particles_function(args...)
+        out = Array{Float64, 1}[]
+        return out
+    end
+
+    return static_particles_function
+end
+
+
+function save_vtk(self::QVLMVehicle, filename; path=nothing, num=nothing,
+                                                save_wopwopin=false, optargs...)
+
+    # Filter optional arguments for wake
+    optargs1 = [(sym, val) for (sym, val) in optargs
+                         if !(sym in [:infinite_vortex, :save_horseshoes,
+                                      :only_horseshoes, :only_infinite_vortex])]
+
+    # Save wake
+    strn = vlm.save(self.wake_system, filename; suff="_wake",
+                                infinite_vortex=true, save_horseshoes=true,
+                                only_horseshoes=true, only_infinite_vortex=true,
+                                path=path, num=num, optargs1...)
+
+    # Filter optional arguments of VLM
+    optargs2 = [(sym, val) for (sym, val) in optargs if sym!=:infinite_vortex]
+
+    # Save VLM
+    strn *= save_vtk_base(self, filename; path=path, num=num,
+                                    save_wopwopin=save_wopwopin,
+                                    infinite_vortex=false, optargs2...)
+    return strn
+end
+##### INTERNAL FUNCTIONS  ######################################################
+
+##### END OF VEHICLE ###########################################################
