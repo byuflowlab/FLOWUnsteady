3fd0055810deb4bc8849fcb6b4e226bff0d6b292
Judd Mehr <juddmehr@gmail.com>
2020-06-08 14:22:01 -0600

    add empty files

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,22 @@ makedocs(
             authors = "Eduardo J. Alvarez, Ryan Anderson, Adam Cardoza, Tyler Critchfield, Judd Mehr",
             sitename="FLOWUnsteady",
             pages = ["Home" => "index.md",
-                    "Theory" => "theory.md",
-                    "Tutorials" => "tutorials.md",
-                    "How-to Guides" => "how-to.md"
+                    "Theory" => "ref/theory.md",
+                    "ref/FLOWVLM.md",
+                    "ref/VPM.md",
+                    "ref/FLOWUnsteady.md"
+                    "Tutorials" => "tutorial/tutorials.md",
+                    "tutorial/geometry-basics.md"
+                    "How-to Guide" => "howto/how-to.md",
+                    "howto/getting-started.md",
+                    "howto/paraview-visualization.md",
+                    "howto/defining-systems.md",
+                    "howto/define-a-rotor.md",
+                    "howto/define-kinematic-maneuvers.md",
+                    "howto/select-a-solver.md",
+                    "howto/calculate-aerodynamic-forces.md",
+                    "howto/set-up-runtime-functions.md"
+                    "howto/set-up-monitors.md",
                     ]
         )
 
