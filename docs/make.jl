0d9bb068a74dc108afb47cfc2c9ed3f613191b91
Judd Mehr <juddmehr@gmail.com>
2020-06-08 11:59:55 -0600

    add clean docs

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using Documenter, FLOWUnsteady
+
+makedocs(
+            root    = ".",
+            source  = "./src",
+            build   = "./build",
+            authors = "Eduardo J. Alvarez, Ryan Anderson, Adam Cardoza, Tyler Critchfield, Judd Mehr",
+            sitename="FLOWUnsteady",
+            pages = ["Home" => "index.md",
+                    "Theory" => "theory.md",
+                    "Tutorials" => "tutorials.md",
+                    "How-to Guides" => "howto.md"
+                    ]
+        )
+
+deploydocs(
+            repo   = "github.com/byuflowlab/FLOWUnsteady",
+            root   = ".",
+            target = "./build",
+            branch = "gh-pages",
+            # devbranch = "master",
+            julia = "v0.6"
+        )
