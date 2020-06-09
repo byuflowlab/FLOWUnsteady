eabb55015b4a557116a8630ec51cea6d52c7ea37
Judd Mehr <juddmehr@gmail.com>
2020-06-09 11:19:10 -0600

    new docs version start

--- docs/make.jl
+++ docs/make.jl
@@ -1,37 +1,33 @@
-using Documenter, FLOWUnsteady
-import FLOWVLM
+using Documenter
+# using FLOWUnsteady
 
 makedocs(
-            root    = ".",
-            source  = "./src",
-            build   = "./build",
-            authors = "Eduardo J. Alvarez, Ryan Anderson, Adam Cardoza, Tyler Critchfield, Judd Mehr",
-            sitename= "FLOWUnsteady",
-            pages = ["Home" => "index.md",
-                    "Reference" => "reference/theory.md",
-                        "reference/FLOWVLM.md",
-                        "reference/VPM.md",
-                        "reference/FLOWUnsteady.md",
-                    "Tutorials" => "tutorials/tutorials.md",
-                        "tutorials/first-steps.md",
-                    "How-to-Guide" => "how-to-guide/how-to.md",
-                        "how-to-guide/getting-started.md",
-                        "how-to-guide/paraview-visualization.md",
-                        "how-to-guide/defining-systems.md",
-                        "how-to-guide/define-a-rotor.md",
-                        "how-to-guide/define-kinematic-maneuvers.md",
-                        "how-to-guide/select-a-solver.md",
-                        "how-to-guide/calculate-aerodynamic-forces.md",
-                        "how-to-guide/set-up-runtime-functions.md",
-                        "how-to-guide/set-up-monitors.md",
-                    ]
-        )
+    sitename = "FLOWUnsteady",
+    format = Documenter.HTML(),
+    # modules = [FLOWUnsteady],
+    pages = ["Home" => "index.md",
+            "Reference" => "reference/theory.md",
+                "reference/FLOWVLM.md",
+                "reference/VPM.md",
+                "reference/FLOWUnsteady.md",
+            "Tutorials" => "tutorials/tutorials.md",
+                "tutorials/first-steps.md",
+            "How-to-Guide" => "how-to-guide/how-to.md",
+                "how-to-guide/getting-started.md",
+                "how-to-guide/paraview-visualization.md",
+                "how-to-guide/defining-systems.md",
+                "how-to-guide/define-a-rotor.md",
+                "how-to-guide/define-kinematic-maneuvers.md",
+                "how-to-guide/select-a-solver.md",
+                "how-to-guide/calculate-aerodynamic-forces.md",
+                "how-to-guide/set-up-runtime-functions.md",
+                "how-to-guide/set-up-monitors.md",
+            ]
+)
 
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
 deploydocs(
-            repo   = "github.com/byuflowlab/FLOWUnsteady",
-            root   = ".",
-            target = "./build",
-            branch = "gh-pages",
-            # devbranch = "master",
-            julia = "0.6"
-        )
+    repo   = "github.com/byuflowlab/FLOWUnsteady",
+)
