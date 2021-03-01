3865be6a56f2c214b6425b4992e36215fcee1de8
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2021-03-01 15:27:51 -0700

    Update Vahana: Adjust removal sphere

--- examples/vahana2/vahana2_misc.jl
+++ examples/vahana2/vahana2_misc.jl
@@ -24,15 +24,15 @@ function remove_particles_lowstrength(sim, PFIELD, T, DT; crit=0.00001^2)
 end
 
 
-function remove_particles_sphere(sim, PFIELD, T, DT; Rsphere2=(1.5*5.86)^2)
+function remove_particles_sphere(sim, PFIELD, T, DT; Rsphere2=(1.25*5.86)^2, Xoff=[4.0, 0, 0])
 
     Xvehicle = sim.vehicle.system.O
 
     for i in vpm.get_np(PFIELD):-1:1
         P = vpm.get_particle(PFIELD, i)
-        X1 = P.X[1] - Xvehicle[1]
-        X2 = P.X[2] - Xvehicle[2]
-        X3 = P.X[3] - Xvehicle[3]
+        X1 = P.X[1] - (Xvehicle[1] - Xoff[1])
+        X2 = P.X[2] - (Xvehicle[2] - Xoff[2])
+        X3 = P.X[3] - (Xvehicle[3] - Xoff[3])
 
         if X1*X1 + X2*X2 + X3*X3 > Rsphere2
             vpm.remove_particle(PFIELD, i)
