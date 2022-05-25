d77a2f2d13d620e2523160ab2cf6c5c4738b640d
rymanderson <rymanderson@gmail.com>
2022-05-25 10:34:35 -0600

    use compute directory

--- examples/ground/write_scripts_20220524.jl
+++ examples/ground/write_scripts_20220524.jl
@@ -33,7 +33,7 @@ end
 # Healy rotor, single rotor, no ground
 rotor_function_no_ground(RPM) =
 """
-run_singlerotor_hover(; xfoil=false, RPM=$RPM, rotor_file="Healy_rotor.csv", prompt=false, save_path=joinpath(extdrive_path,"singlerotor_hover_test01/"))
+run_singlerotor_hover(; xfoil=false, RPM=$RPM, rotor_file="Healy_rotor.csv", prompt=false, save_path="$save_path")
 """
 
 rotor_function_panel_ground(tag, h_over_d, dx, nx, RPM) =
