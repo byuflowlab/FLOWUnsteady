6943a00d677ebe890472dff7cf38d9e4b5d8d7a5
rymanderson <rymanderson@gmail.com>
2022-05-25 13:45:30 -0600

    more flexible file writer

--- examples/ground/write_scripts_20220524.jl
+++ examples/ground/write_scripts_20220524.jl
@@ -12,18 +12,19 @@ uns = FLOWUnsteady
 function write_script_20220524(name="healy_";
         write_dir = save_path,
         # script_path = joinpath(save_path, name*".jl"),
-        script_names = name .* ["noground_npr72", "panels_hd05_dx3_nx10_npr72","panels_hd1_dx3_nx10_npr72","panels_hd05_dx6_nx20_npr72","panels_hd1_dx6_nx20_npr72"] .* ".jl",
+        get_fun_list = [rotor_function_images_ground, rotor_function_images_ground, rotor_function_panel_ground, rotor_function_panel_ground, rotor_function_panel_ground, rotor_function_panel_ground]
+        script_names = name .* ["noground_npr72", "images_ground_hd05_npr72", "images_ground_hd1_npr72", "panels_hd05_dx3_nx10_npr72","panels_hd1_dx3_nx10_npr72","panels_hd05_dx6_nx20_npr72","panels_hd1_dx6_nx20_npr72"] .* ".jl",
         example_file = joinpath("ground", "rotors.jl"),
-        include_no_ground=true,
+        include_no_ground=true, include_images_ground=true,
         FLOWExaFMM_dir=joinpath(ENV["HOME"], "julia", "dev", "FLOWExaFMM"),
         RPM = 1600.0,
-        hd_list = ["0.5", "1.0", "0.5", "1.0"],
-        dx_list = ["3.0", "3.0", "6.0", "6.0"],
-        nx_list = ["10", "10", "20", "20"],
-        npr_list = ["72", "72", "72", "72"],
+        hd_list = ["0.5", "1.0", "0.5", "1.0", "0.5", "1.0"],
+        dx_list = ["3.0", "3.0", "3.0", "3.0", "6.0", "6.0"],
+        nx_list = ["10", "10", "10", "10", "20", "20"],
+        npr_list = ["72", "72", "72", "72", "72", "72"],
     )
 
-    call_example_functions = get_function_strings(RPM, hd_list, dx_list, nx_list, npr_list; include_no_ground)
+    call_example_functions = get_function_strings(get_fun_list, RPM, hd_list, dx_list, nx_list, npr_list; include_no_ground)
     @assert length(script_names) == length(hd_list) + include_no_ground
     @assert length(call_example_functions) == length(script_names)
     for (i,call_example_function) in enumerate(call_example_functions)
@@ -31,7 +32,7 @@ function write_script_20220524(name="healy_";
     end
     uns.write_batch(joinpath(write_dir, name*"batch.sh"), script_names, FLOWExaFMM_dir;
         test=true, email_notifications=false, email_address="",
-        time="01:00:00", ntasks=24, mem_per_cpu="1024M"
+        time="08:00:00", ntasks=16, mem_per_cpu="1024M"
     )
     return nothing
 end
@@ -42,7 +43,7 @@ rotor_function_no_ground(RPM, nsteps_per_rev) =
 run_singlerotor_hover(; xfoil=false, RPM=$RPM, rotor_file="Healy_rotor.csv", prompt=false, save_path=joinpath("$save_path","single_rotor_hover_ground_none"), nrevs=14, nsteps_per_rev=$nsteps_per_rev)
 """
 
-rotor_function_panel_ground(tag, h_over_d, dx, nx, RPM, nsteps_per_rev) =
+rotor_function_panel_ground(tag, h_over_d, RPM, nsteps_per_rev, dx, nx) =
 """
 run_singlerotor_hover_ground_panel(; run_name="healy_rotor_panel"*$tag, xfoil=false, prompt=false, disp_conv=true,
         ground_point = [5.5*0.3048 * $h_over_d,0,0], ground_axes = [0.0 0.0 1.0; 0.0 1.0 0.0; -1.0 0 0.0],
@@ -50,20 +51,20 @@ run_singlerotor_hover_ground_panel(; run_name="healy_rotor_panel"*$tag, xfoil=fa
         kernel = PS.Source(), panel_shape = PS.Quad(),
         RPM = $RPM,
         nrevs = 14, nsteps_per_rev = $(nsteps_per_rev),
-        save_path=joinpath("$save_path","single_rotor_hover_ground_panel"),
+        save_path=joinpath("$save_path","healy_panel_hd$(h_over_d)_dx$(dx)_nx$(nx)_npr$(nsteps_per_rev)"),
     )
 """
 
-rotor_function_images_ground(tag, h_over_d, RPM, nsteps_per_rev) =
+rotor_function_images_ground(tag, h_over_d, RPM, nsteps_per_rev, args...) =
 """
 run_singlerotor_hover_ground_mirror(; run_name="rotor_mirror"*$tag, xfoil=false, prompt=false, disp_conv=false,
         ground_point = [5.5*0.3048 * $h_over_d,0,0], ground_normal = [-1.0, 0,0], RPM = $RPM,
         nrevs = 14, nsteps_per_rev = $(nsteps_per_rev),
-        save_path=joinpath("$save_path","single_rotor_hover_ground_images"),
+        save_path=joinpath("$save_path","healy_images_hd$(h_over_d)_npr$(nsteps_per_rev)"),
     )
 """
 
-function get_function_strings(RPM, hd_list, dx_list, nx_list, npr_list; include_no_ground=true)
+function get_function_strings(get_fun_list, RPM, hd_list, dx_list, nx_list, npr_list; include_no_ground=true)
     ex_funs = String[]
     if include_no_ground; push!(ex_funs, rotor_function_no_ground(RPM, parse(Int64,npr_list[1]))); end
     for i in 1:length(hd_list)
@@ -71,7 +72,7 @@ function get_function_strings(RPM, hd_list, dx_list, nx_list, npr_list; include_
         dx = dx_list[i]
         nx = nx_list[i]
         npr = npr_list[i]
-        push!(ex_funs, rotor_function_panel_ground("_hd$(hd)_dx$(dx)_nx$(nx)_npr$npr", parse(Float64,hd), parse(Float64,dx), parse(Int64,nx), RPM, parse(Int64,npr)))
+        push!(ex_funs, get_fun_list[i]("_hd$(hd)_dx$(dx)_nx$(nx)_npr$npr", parse(Float64,hd), RPM, parse(Int64,npr), parse(Float64,dx), parse(Int64,nx)))
     end
     return ex_funs
 end
